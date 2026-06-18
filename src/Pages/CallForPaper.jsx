0f9a116e4249ef2e0a6b4e9fa7d91e9605c398a6
Dhilip Kumar <dhilipmari916@gmail.com>
2026-06-18 11:26:47 +0530

    cfp updtae123

--- src/Pages/CallForPaper.jsx
+++ src/Pages/CallForPaper.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer"; // Footer import பண்ணிருக்கோம்
+import Footer from "../components/Footer.jsx"; // Footer import பண்ணிருக்கோம்
 
 export default function CallForPapers() {
   const [showCmtPopup, setShowCmtPopup] = useState(false);
